115d51b43b0c04a4fdbeb2810a1296b263b77010
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2020-02-11 18:46:36 -0300

    Agrego la app principal del proyecto

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
 const express = require('express')
+const logger = require('morgan')
+const admin = require('firebase-admin')
+const bodyParser = require('body-parser')
+
 const app = express()
-const port = 3000
+const port = Number(process.env.port || 3030)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var idToken;
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+/*
+    Windows PowerShell
+    $env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Rodrigo Villarreal\Desarrollo\NodeJs\challenge-nodejs-chrr\service-account-file.json"
+    Linux Terminal
+    export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
+*/
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://nodechallenge-424c1.firebaseio.com'
+  });
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendfile('./index.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendfile('./login.html');
+})
+
+app.post('/login', (req, res) => {
+    idToken = req.body.token;
+    console.log(idToken);
+})
+
+
+
+//
+
+app.listen(port, () => console.log(`Challenge App listening on port:`, port));
+
